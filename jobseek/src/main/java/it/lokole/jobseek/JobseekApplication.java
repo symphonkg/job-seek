2fb4e84091bd1effd1c0558939dc8ec68a1961d7
Sympho Nkongolo Kabua Ka Ntanda <symphonkongolo@hotmail.com>
2020-08-08 08:30:18 +0200

    project first creation and commit: containing only spring boot project

---
+++ jobseek/src/main/java/it/lokole/jobseek/JobseekApplication.java
@@ -0,0 +1,13 @@
+package it.lokole.jobseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobseekApplication.class, args);
+	}
+
+}
---
+++ jobseek/src/test/java/it/lokole/jobseek/JobseekApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lokole.jobseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
