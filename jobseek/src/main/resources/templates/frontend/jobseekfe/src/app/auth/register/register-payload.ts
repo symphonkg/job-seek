8a43f422ffa5d0248a0f9ce847bc27b99c2463f6
Sympho Nkongolo Kabua Ka Ntanda <symphonkongolo@hotmail.com>
2020-11-27 11:56:13 +0100

    aggiunta model per la gestione dei dati in ingresso verso il webservice

---
+++ jobseek/src/main/resources/templates/frontend/jobseekfe/src/app/auth/register/register-payload.ts
@@ -0,0 +1,8 @@
+export class RegisterPayload { 
+    name: String = '';
+    surname: String = '';
+    username: String = '';
+    email: String = '';
+    password: String = '';
+    confirmPassword: String = ''; 
+}
