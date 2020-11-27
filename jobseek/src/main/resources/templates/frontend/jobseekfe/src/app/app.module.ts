4d7bb27ef0db936e878c83023d44ba10cbb5b8c3
Sympho Nkongolo Kabua Ka Ntanda <symphonkongolo@hotmail.com>
2020-11-27 11:55:03 +0100

    creazione form di registrazione, e la classe model RegisterPayload per gestire i dati in input

--- jobseek/src/main/resources/templates/frontend/jobseekfe/src/app/app.module.ts
+++ jobseek/src/main/resources/templates/frontend/jobseekfe/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +17,13 @@ import { RegisterSuccessComponent } from './auth/register-success/register-succe
     FooterComponent,
     RegisterComponent,
     LoginComponent,
-    RegisterSuccessComponent
+    RegisterSuccessComponent  
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, // BrowserModule è il componente base per lavorare con i template HTML nel browser. Grazie a questo modulo è possibile compilare direttive come NgIf e NgFor (anche se in realtà fanno parte del modulo CommonModule che è importato e riesportato come dipendenza)
+    AppRoutingModule, // permette di passare da una directory add un altra, da un path all'altro
+    FormsModule, // abilita ngModel e RouterLink e permette di creare form nello stile “template-driven” (cioè il modo che abbiamo sempre usato in AngularJS). E’ stato introdotto un nuovo tipo di form chiamato “reactive” o “model-driven“, ovvero form create dinamicamente a partire da un modello di metadati che le descrivono. Da tenere sott’occhio perché evolverà parecchio nelle prossime versioni di Angular
+    ReactiveFormsModule // abilita ngModel e RouterLink e permette di creare form nello stile “template-driven” (cioè il modo che abbiamo sempre usato in AngularJS). E’ stato introdotto un nuovo tipo di form chiamato “reactive” o “model-driven“, ovvero form create dinamicamente a partire da un modello di metadati che le descrivono. Da tenere sott’occhio perché evolverà parecchio nelle prossime versioni di Angular
   ],
   providers: [],
   bootstrap: [AppComponent]
--- jobseek/src/main/resources/templates/frontend/jobseekfe/src/app/auth/register/register.component.ts
+++ jobseek/src/main/resources/templates/frontend/jobseekfe/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterPayload } from './register-payload';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  registerPayload : RegisterPayload;
 
+  constructor(private formBuilder: FormBuilder) {
+    
+    this.registerForm = this.formBuilder.group({
+      name: '',
+      surname: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''});
+
+    this.registerPayload = {
+      name: '',
+      surname: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    } 
+  }
+ 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.registerPayload.name =  this.registerForm.get('name')?.value;
+    this.registerPayload.surname = this.registerForm.get('surname')?.value;
+    this.registerPayload.username = this.registerForm.get('username')?.value;
+    this.registerPayload.email = this.registerForm.get('email')?.value; 
+    this.registerPayload.password = this.registerForm.get('password')?.value;
+    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword')?.value;
   }
 
 }
